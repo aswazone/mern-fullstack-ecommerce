1c7de16b1c17a203002aca7596937e2390a17821
aswazone <aswamanu143@gmail.com>
2025-03-06 16:17:51 +0530

    feat: Added the AdminHeader, AdminSidebar, responsiveness and functionality of sidebar by useState & useNavigate (using Shadcn -> Sheet)

--- client/src/components/admin-view/Header.jsx
+++ client/src/components/admin-view/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import { LogOut, Menu } from 'lucide-react'
 
-const AdminHeader = () => {
-  return (
-    <div>AdminHeader</div>
-  )
+const AdminHeader = ({setOpen}) => {
+  return <header className='flex item-center justify-between px-4 py-3 bg-background border-b'>
+    <Button onClick={()=>setOpen(true)} className='lg:hidden sm:block'>
+      <Menu />
+      <span className='sr-only'>Toggle Menu</span>
+    </Button>
+    <div className="flex flex-1 justify-end">
+      <Button className='inline-flex gap-2 items-center rounded px-4 py-2 text-sm font-medium shadow'>
+        <LogOut />Logout
+      </Button>
+    </div>
+  </header>
 }
 
 export default AdminHeader
--- client/src/components/admin-view/Sidebar.jsx
+++ client/src/components/admin-view/Sidebar.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import { LayoutDashboard, PackageCheck, Shirt } from "lucide-react"
+import { ShieldUser } from 'lucide-react'
+import React, { Fragment } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle } from "../ui/sheet"
 
-const AdminSidebar = () => {
-  return (
-    <div>AdminSidebar</div>
-  )
+const adminSidebarMenuItems = [
+  {
+      id : 'dashboard',
+      label : 'Dashboard',
+      path : '/admin/dashboard',
+      icon : <LayoutDashboard />
+  },
+  {
+      id : 'products',
+      label : 'Products',
+      path : '/admin/products',
+      icon : <Shirt />
+  },
+  {
+      id : 'orders',
+      label : 'Orders',
+      path : '/admin/orders',
+      icon : <PackageCheck />
+  },
+]
+
+const MenuItems = ({setOpen}) =>{
+
+  const navigate = useNavigate();
+
+  return <nav className='mt-8 flex-col flex gap-2'>
+    {
+      adminSidebarMenuItems.map((item) => (
+        <div 
+          key={item.id} 
+          onClick={()=> {
+            navigate(item.path)
+            setOpen ? setOpen(false) : null
+          }} 
+          className='flex font-medium cursor-pointer items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground'>{item.icon} <span>{item.label}</span>
+        </div>
+        ))
+    }
+  </nav>
+}
+
+
+const AdminSidebar = ({open, setOpen}) => {
+  const navigate = useNavigate();
+
+  return <Fragment>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetContent side="left" className='w-64'>
+          <div className="flex flex-col h-full">
+            <SheetHeader className='border-b'>
+              <SheetTitle className='flex gap-2'>
+                <ShieldUser size={30} />
+                <h1 className='text-xl font-bold'>Admin Panel</h1>
+              </SheetTitle>
+            </SheetHeader>
+            <MenuItems setOpen={setOpen}/>
+          </div>
+        </SheetContent>
+      </Sheet>
+      <aside className='hidden w-64 flex-col border-r bg-background p-6 lg:flex'>
+        <div onClick={()=> navigate('/admin/dashboard')} className='flex cursor-pointer items-center gap-2'>
+          <ShieldUser size={30} />
+          <h1 className='text-xl font-bold'>Admin Panel</h1>
+        </div>
+        <MenuItems />
+      </aside>
+    </Fragment>
 }
 
 export default AdminSidebar
